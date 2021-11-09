43ed439b44c27395029e994f63e4ad614280ee69
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-11-09 01:39:34 +0000

    users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:username", { :controller => "users", :action => "user_details"})
+
+  get("/", { :controller => "users", :action => "index" })
+
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:photo_id", { :controller => "photos", :action => "photo_details"})
 
 end
